f283356baae472e0cec5d335bd1a70daa3d6ef86
isiwoo <isiwoo09s@gmail.com>
2025-04-11 17:56:08 +0900

    multiply and divide

--- main3.py
+++ main3.py
@@ -3,9 +3,3 @@ def multiply(x,y):
 
 def divide(x,y):
     return x/y
-x, y = map(int, input("x y 형태로 입력하세요: ").split())
-print("x =", x)
-print("y =", y)
-
-print(multiply(x,y))
-print(divide(x,y))
